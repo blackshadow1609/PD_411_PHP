da6616336cbc95ccd21a5ecf67354f936de4d0f9
Blackshadow <blackshadow0916@yandex.ru>
2026-07-17 19:32:01 +0300

    HW_PHP_03_07_26 DONE

--- 04-quitz/inc/quitz.php
+++ 04-quitz/inc/quitz.php
@@ -1,10 +1,14 @@
 <?php
 require_once __DIR__ . '/header.php';
-echo "Привет, {$_POST['first_name']} {$_POST['last_name']}, приятного прохождения!";
-echo '<pre>';
-print_r(ROOT . "\n");
-echo $_SERVER['DOCUMENT_ROOT'];
-echo '</pre>';
+
+// Проверка
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['first_name']) && isset($_POST['last_name'])) {
+    echo "Привет, {$_POST['first_name']} {$_POST['last_name']}, приятного прохождения!";
+    echo '<pre>';
+    print_r(ROOT . "\n");
+    echo $_SERVER['DOCUMENT_ROOT'];
+    echo '</pre>';
+}
 ?>
 
 <form action="" method="post">
@@ -13,15 +17,42 @@ echo '</pre>';
             <div class="question">
                 <h3><?= $questions[$i] ?></h3>
                 <?php for ($j = 0; $j < count($answers[$i]); $j++): ?>
-                    <input type="radio" name="<?= $questions[$i] ?>" id="<?= "{$i}_{$j}" ?>" value="<?= "{$i}_{$j}" ?>" />
+                    <input type="radio" name="question_<?= $i ?>" id="<?= "{$i}_{$j}" ?>" value="<?= $j ?>" />
                     <label for="<?= "{$i}_{$j}" ?>"><?= $answers[$i][$j] ?></label>; <br />
                 <?php endfor ?>
             </div>
         <?php endfor ?>
     </div>
-    <input type="submit" value="Отправить" />
+    
+    <div class="submit-button">
+        <input type="submit" value="Отправить" />
+    </div>
 </form>
 
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['question_0'])) {
+    $correct_count = 0;
+    $wrong_count = 0;
+
+    for ($i = 0; $i < count($questions); $i++) {
+        if (isset($_POST["question_$i"])) {
+            $user_answer = (int) $_POST["question_$i"];
+            if ($user_answer === $correct_answers[$i]) {
+                $correct_count++;
+            } else {
+                $wrong_count++;
+            }
+        } else {
+            $wrong_count++; 
+        }
+    }
+
+    echo '<div class="results">';
+    echo "<p>Правильных ответов: $correct_count</p>";
+    echo "<p>Неправильных ответов: $wrong_count</p>";
+    echo '</div>';
+}
+
 require_once __DIR__ . '/footer.php';
 ?>
